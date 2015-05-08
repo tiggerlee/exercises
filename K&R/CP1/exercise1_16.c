877a6e82de7e7cf5516dfc87a372f0075b5ec8a2
tiggerlee <xiahulee@gmail.com>
2015-05-08 14:57:11 +0800

    Finished exercise 1.16 only can display maxlength version

---
+++ K&R/CP1/exercise1_16.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getl(char s[], int lim);
+void copy(char to[], char from[]);
+
+int main()
+{
+  int max, len;
+  char line[MAXLINE];
+  char longest[MAXLINE];
+
+  max = 0;
+
+  while ((len = getl(line, MAXLINE)) > 0) {
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    }
+  }
+
+  if (max > 0) {
+    printf("Max line is: %s", longest);
+  }
+  return 0;
+}
+
+int getl(char s[], int lim)
+{
+  int c, i;
+
+  for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+
+  s[i] = '\0';
+
+  return i;
+}
+
+void copy(char to[], char from[])
+{
+  int i;
+
+  i = 0;
+  while ((to[i] = from[i]) != '\0')
+    ++i;
+}
