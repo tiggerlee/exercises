955007b926b2ffd4bdfeeb16e642e4acaab30f6f
tiggerlee <xiahulee@gmail.com>
2015-06-04 16:31:24 +0800

    Add comment for solution
    加入解题思路和步骤的注释

--- CSAPP/cp2/exercise2_60.c
+++ CSAPP/cp2/exercise2_60.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+/*
+ * 以put_byte(0x12345678, 0xFF, 2) 为例：
+ * 我们想要将第一个参数左起第三个字节的值(34)替换为FF
+ * 首先构造两个数值：0x12000000 和 0x00005678，这样我们就可以利用这两个值和0xFF， 利用 | 操作符连接，得到我们需要的0x12FF5678
+ */
 unsigned put_byte(unsigned x, unsigned char b, int i) {
+  /* 0xFF000000 */
   unsigned mark = 0xFFFFFFFF << ((i + 1) * 8);
+  /* 得到我们要构造的第一个数值 */
   unsigned as1 = x & mark;
+  /* 0xFFFF0000 */
   mark = 0xFFFFFFFF << (i * 8);
+  /* 得到我们要构造的第二个数值 */
   unsigned as2 = x ^ (x & mark);
+  /* 将要替换的数值执行右移操作，移动到要被替换位置 */
   unsigned place = b << (i * 8);
   unsigned result = as1 | as2 | place;
   return result;
--- CSAPP/cp2/exercise2_62.c
+++ CSAPP/cp2/exercise2_62.c
@@ -1,5 +1,8 @@
 int int_shifts_are_logical() {
+  /* 得到一个所有位都为1的数 */
   int x = ~0;
+  /* 如果是算数右移，所有位应该还是1 */
   int shifted_val = x >> 1;
+  /* 算数右移之后的结果应该和之前的相等 */
   return x == shifted_val;
 }
