b406a2dafc29083d51e2b1a05c69fca59b794852
tiggerlee <xiahulee@gmail.com>
2017-08-10 16:45:52 +0800

    modify exercise 1.20

--- SICP/cp1/exercise1_20.scm
+++ SICP/cp1/exercise1_20.scm
@@ -7,23 +7,23 @@
 
 (gcd 206 40)
 
-; normal-order
-; (gcd 40 (remainder 206 40)) 1
-; (gcd 40 6)
-
-; (gcd 6 (remainder 40 6))    2
-; (gcd 6 4)
-
-; (gcd 4 (remainder 6 4))     3
-; (gcd 4 2)
-
-; (gcd 2 (remainder 4 2))     4
-; (gcd 2 0)
-
-
-; applicative-order
+; 应用序调用了4次remainder函数
+; (gcd 206 40)
 ; (gcd 40 (remainder 206 40))
-; (gcd 40 (remainder 206 40)
-;   (if (= (remainder 206 40) 0)))
+; (gcd 40 6)
+; (gcd 6 (remainder(40 6)))
+; (gcd 6 4)
+; (gcd 4 (remainder 6 4))
+; (gcd 4 2)
+; (gcd 2 (reminder 4 2))
+; (gcd 2 0)
+; 2
+
+
+; 正则序， 因为先展开再求值得特性，remainder函数调用远远超过了应用序中的调用。
+; (gcd 206 40)
+; gcd(gcd 40 (remainder 206 40))
+; if (= (remainder(206 40))
 ; (gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+; (if (= (remainder 40 (remainder 206 40)) 0))
 ; ...
