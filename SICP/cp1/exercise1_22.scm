e0b3204c7bfe6a007fe9dde5c9a07026bf2e9b5f
tiggerlee <xiahulee@gmail.com>
2017-08-11 17:39:21 +0800

    modify exercise 1.22

--- SICP/cp1/exercise1_22.scm
+++ SICP/cp1/exercise1_22.scm
@@ -1,16 +1,15 @@
 #lang sicp
 
+(define (square x)
+  (* x x))
 
-; 在我的计算机上运行结果如最下面的注释所示
-; 结果为 1 3 8 17
-; 刚开始比较接近(sqrt 10)的增长速度，随着数字增加误差开始增大
 (define (smallest-divisor n)
   (find-divisor n 2))
 
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
-	((divides? test-divisor n) test-divisor)
-	(else (find-divisor n (+ 1 test-divisor)))))
+             ((divides? test-divisor n) test-divisor)
+             (else (find-divisor n (+ test-divisor 1)))))
 
 (define (divides? a b)
   (= (remainder b a) 0))
@@ -18,57 +17,15 @@
 (define (prime? n)
   (= n (smallest-divisor n)))
 
-(define (next-odd n)
-  (if (odd? n)
-      (+ n 2)
-      (+ n 1)))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
 
-(define (find-prime n c)
-  (let ((a 0))
-    (cond ((= a c) (display "Done"))
-	  ((prime? n)
-	   (display n)
-	   (newline)
-	   (find-prime (next-odd n) (- c 1)))
-	  (else (find-prime (next-odd n) c)))))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
 
 (define (time-prime-test n)
-  (let ((start-time (real-time-clock)))
-    (find-prime n 3)
-    (newline)
-    (display (- (real-time-clock) start-time))))
-
-
-; 1 ]=> (load "exercise1_22.scm")
-
-
-; 1 ]=> (time-prime-test 1000)
-; 1009
-; 1013
-; 1019
-; Done
-; 1
-
-
-; 1 ]=> (time-prime-test 10000)
-; 10007
-; 10009
-; 10037
-; Done
-; 3
-
-
-; 1 ]=> (time-prime-test 100000)
-; 100003
-; 100019
-; 100043
-; Done
-; 8
-
-
-; 1 ]=> (time-prime-test 1000000)
-; 1000003
-; 1000033
-; 1000037
-; Done
-; 17
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
