69b324a4d22326293e3bae9780bca37589bea4f4
tiggerlee <xiahulee@gmail.com>
2017-08-21 19:41:36 +0800

    modify exercise 1.30 1.31

--- SICP/cp1/exercise1_30.scm
+++ SICP/cp1/exercise1_30.scm
@@ -1,6 +1,38 @@
+#lang sicp
+
+(define (cube x)
+  (* x x x))
+
 (define (sum term a next b)
   (define (iter a result)
     (if (> a b)
-	result
-	(iter (next a) (+ result (term a)))))
+        result
+        (iter (next a) (+ result (term a)))))
   (iter a 0))
+
+(define (integral f a b dx)
+  (define (add-dx x) 
+    (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
+
+(define (simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (y k)
+    (f (+ a (* k h))))
+  (define (factor k)
+    (cond ((or (= k 0) (= k n)) 1)
+	        ((odd? k) 4)
+	        (else 2)))
+  (define (term k)
+    (* (factor k)
+       (y k)))
+  (if (not (even? n))
+      (display "n can't be odd")
+      (* (/ h 3)
+         (sum term (exact->inexact 0) inc n))))
+                
+(integral cube 0 1 0.01) ; 0.24998750000000042
+(integral cube 0 1 0.001); 0.249999875000001
+(simpson cube 0 1 100)   ; 0.24999999999999992
+(simpson cube 0 1 1000)  ; 0.2500000000000003
+(simpson cube 0 1 999)   ; n can't be odd
