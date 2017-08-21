fb8b546016bbd312bcc566885795143e9720ac33
tiggerlee <xiahulee@gmail.com>
2017-08-21 19:41:36 +0800

    modify exercise 1.30 1.31

--- SICP/cp1/exercise1_31.scm
+++ SICP/cp1/exercise1_31.scm
@@ -1,30 +1,42 @@
-; a
-(define (product term a next b)
+#lang sicp
+
+; 递归版本
+(define (product-recu term a next b)
   (if (> a b)
       1
       (* (term a)
-	 (product term (next a) next b))))
+         (product-recu term (next a) next b))))
 
-(define (fac n)
-  (define (term x) x)
-  (define (next x) (+ x 1))
-  (product term 1 next n))
-
-(define (next n)
-  (+ n 1))
-
-(define (pi-term n)
-  (if (even? n)
-      (/ (+ n 2) (+ n 1))
-      (/ (+ n 1) (+ n 2))))
-
-(define (pi n)
-  (* 4.0 (product-i pi-term 1 next n)))
-
-; b
-(define (product-i term a next b)
+; 迭代版本
+(define (product term a next b)
   (define (iter a result)
     (if (> a b)
-	result
-	(iter (next a) (* result (term a)))))
+        result
+        (iter (next a) (* result (term a)))))
   (iter a 1))
+
+(define (identity x) x)
+
+(define (factorial n)
+  (product identity 1 inc n))
+
+(factorial 4)  ; 24
+(factorial 7)  ; 5040
+(factorial 10) ; 3628800
+
+
+; 解答参考了 http://sicp.readthedocs.io/en/latest/chp1/31.html
+(define (pi n)
+  (define (numerator x)
+    (cond ((even? x) (+ x 2))
+          (else (+ x 1))))
+  (define (denominator x)
+    (cond ((even? x) (+ x 1))
+          (else (+ x 2))))
+  (* 4 (exact->inexact (/ (product numerator 1 inc n)
+                          (product denominator 1 inc n)))))
+
+(pi 100)     ; 3.1570301764551676
+(pi 1000)    ; 3.1431607055322663
+(pi 10000)   ; 3.1417497057380523
+(pi 100000)  ; 3.1416083612781764
