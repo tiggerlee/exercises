7730ef686549c9836d816a3f35527411c6912096
tiggerlee <xiahulee@gmail.com>
2017-08-22 10:33:48 +0800

    modify exercise 1.32 1.33

--- SICP/cp1/exercise1_32.scm
+++ SICP/cp1/exercise1_32.scm
@@ -1,26 +1,34 @@
-; a
-(define (next n)
-  (+ n 1))
+#lang sicp
 
+; 迭代版本
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a)
+        (combiner result (term a)))))
+  (iter a null-value))
+
+; 递归版本
 (define (accumulate combiner null-value term a next b)
   (if (> a b)
       null-value
       (combiner (term a)
-		(accumulate combiner null-value term (next a) next b))))
-
-(define (term x) x)
+        (accumulate combiner null-value term (next a) next b))))
 
 (define (sum term a next b)
   (accumulate-iter + 0 term a next b))
 
+(define (cube x)
+  (* x x x))
+
+(define (identity x) x)
+
+(sum identity 1 inc 10)  ; 55
+(sum cube 1 inc 5)       ; 225
+
 (define (product term a next b)
   (accumulate-iter * 1 term a next b))
 
-
-(define (accumulate-iter combiner null-value term a next b)
-  (define (iter a result)
-    (if (> a b)
-	result
-	(iter (next a) (combiner result a))))
-  (iter a null-value))
-
+(product identity 1 inc 10)  ; 3628800
+(product cube 1 inc 10)      ; 47784725839872000000
--- SICP/cp1/exercise1_33.scm
+++ SICP/cp1/exercise1_33.scm
@@ -1,45 +1,56 @@
-(load "exercise1_28.scm")
+#lang sicp
+(#%require (lib "27.ss" "srfi"))
 
-(define (prime? x)
-  (miller-rabin-test x))
+(define (square n)
+	(* n n))
 
-(define (filtered-accumulate combiner null-value term a next b filter)
-  (if (> a b)
-      null-value
-      (if (filter a)
-	  (combiner (term a)
-		    (filtered-accumulate combiner
-					 null-value
-					 term
-					 (next a)
-					 next
-					 b
-					 filter))
-	  (combiner null-value
-		    (filtered-accumulate combiner
-					 null-value
-					 term
-					 (next a)
-					 next
-					 b
-					 filter)))))
+(define (miller-rabin-expmod base exp m)
+  (cond ((= exp 0) 1)
+				((nontrivial-square-root? base m) 0)
+				((even? exp)
+	 				(remainder (square (miller-rabin-expmod base (/ exp 2) m))
+		    						 m))
+				(else
+	 				(remainder (* base (miller-rabin-expmod base (- exp 1) m))
+		   							 m))))
 
-(define (sum-primes a b)
-  (define (term x) x)
-  (define (next x) (+ x 1))
-  (filtered-accumulate + 0 term a next b prime?))
+(define (nontrivial-square-root? base m)
+  (and (not (= base 1))
+       (not (= base (- m 1)))
+       (= 1 (remainder (square base) m))))
 
-(define (gcd a b)
-  (if (= b 0)
-      a
-      (gcd b (remainder a b))))
+(define (prime? n)
+  (define (iter times)
+    (cond ((= times 0) #t)
+	  			((= (miller-rabin-expmod (+ 1 (random-integer (- n 1))) (- n 1) n) 1) (iter (- times 1)))
+	  			(else #f)))
+  (iter (ceiling (/ n 2))))
 
-(define (sum-gcd-filter a b)
-  (if (< a b)
-      (= (gcd a b) 1)
-      #f))
+(prime? 13)   ; #t
+(prime? 100)  ; #f
 
-(define (sum-gcd-one a b)
-  (define (term x) x)
-  (define (next x) (+ x 1))
-  (filtered-accumulate * 1 term a next b (lambda (x) (sum-gcd-filter x b))))
+(define (filter-accumulate filter combiner null-value term a next b)
+  (cond ((> a b) null-value)
+        ((filter a) (combiner (term a)
+                              (filter-accumulate filter combiner null-value term (next a) next b)))
+        (else (filter-accumulate filter combiner null-value term (next a) next b))))
+
+(define (identity x) x)
+
+(define (sum-prime a b)
+  (filter-accumulate prime? + 0 identity a inc b))
+
+; 计算素数之和
+(sum-prime 2 10) ; 17
+
+(define (product-prime n)
+  ; 计算两个数的最大公约数是否为1
+  (define (isgcd1 x)
+    (define (gcd x y)
+      (if (= y 0)
+          x
+          (gcd y (remainder x y))))
+    (= (gcd x n) 1))
+  (filter-accumulate isgcd1 * 1 identity 1 inc n))
+
+(product-prime 10) ; 189
