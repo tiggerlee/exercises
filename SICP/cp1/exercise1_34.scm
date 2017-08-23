88773d8aab146cf0f42d71f9da607c121d3661d9
tiggerlee <xiahulee@gmail.com>
2017-08-23 15:13:50 +0800

    modify exercise 1.34 1.35 1.36

--- SICP/cp1/exercise1_34.scm
+++ SICP/cp1/exercise1_34.scm
@@ -1,7 +1,17 @@
-(define (f g)
-  (g 2))
+#lang sicp
 
+(define (square x) (* x x))
+
+(define (f g) (g 2))
+
+(f square)
+;((lambda (x) (* x x)) 2)
+(f (lambda (z) (* z (+ z 1))))
+; 解答参考了 http://sicp.readthedocs.io/en/latest/chp1/34.html
+; 在racket中显示由于2不是一个procedure而出错
+; 为什么出现错误，可以展开这个表达式
 (f f)
-; 在MIT-scheme中会报错：The object 2 is not applicable.
-; (f f) -> (f 2) -> (2 2)
-; 而2不是一个运算符
+; 首先使用替代法展开(f f)
+;((lambda (x) (x 2)) 2)
+; 将2作为参数传入，lambda会将2作为一个操作符应用于参数之上求值，导致上述错误
+;(2 2)
--- SICP/cp1/exercise1_36.scm
+++ SICP/cp1/exercise1_36.scm
@@ -1,22 +1,74 @@
+#lang sicp
+
 (define tolerance 0.00001)
 
 (define (fixed-point f first-guess)
   (define (close-enough? v1 v2)
-    (< (abs (- v1 v2)) tolerance))
+    (< (abs (- v1 v2))
+        tolerance))
   (define (try guess)
     (display guess)
     (newline)
     (let ((next (f guess)))
       (if (close-enough? guess next)
-	  next
-	  (try next))))
+          next
+          (try next))))
   (try first-guess))
 
-(define (f x)
-  (fixed-point (lambda (x) (/ (log 1000) (log x))) x))
+(define formlate (lambda (x) (/ (log 1000) (log x))))
 
-(define (average x y)
-  (/ (+ x y) 2.0))
+(define  without-average-damping (fixed-point formlate 2.0))
 
-(define (f-with-average x)
-  (fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) x))
+(define average (lambda (x y) (/ (+ x y) 2)))
+
+; 解答参考了 http://sicp.readthedocs.io/en/latest/chp1/36.html
+(define  with-average-damping (fixed-point (lambda (x) (average x (formlate x))) 2.0))
+
+; 根据显示的结果可以看出，使用了 average damping 明显减少了运算时间，加快了函数的收敛速度
+without-average-damping
+; 2.0
+; 9.965784284662087
+; 3.004472209841214
+; 6.279195757507157
+; 3.759850702401539
+; 5.215843784925895
+; 4.182207192401397
+; 4.8277650983445906
+; 4.387593384662677
+; 4.671250085763899
+; 4.481403616895052
+; 4.6053657460929
+; 4.5230849678718865
+; 4.577114682047341
+; 4.541382480151454
+; 4.564903245230833
+; 4.549372679303342
+; 4.559606491913287
+; 4.552853875788271
+; 4.557305529748263
+; 4.554369064436181
+; 4.556305311532999
+; 4.555028263573554
+; 4.555870396702851
+; 4.555315001192079
+; 4.5556812635433275
+; 4.555439715736846
+; 4.555599009998291
+; 4.555493957531389
+; 4.555563237292884
+; 4.555517548417651
+; 4.555547679306398
+; 4.555527808516254
+; 4.555540912917957
+with-average-damping
+; 2.0
+; 5.9828921423310435
+; 4.922168721308343
+; 4.628224318195455
+; 4.568346513136242
+; 4.5577305909237005
+; 4.555909809045131
+; 4.555599411610624
+; 4.5555465521473675
+; 4.555532270803653
+; 4.555537551999825
