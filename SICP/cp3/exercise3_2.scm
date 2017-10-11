d26bb033361866164fe33c375e5bbf08dacb77f5
tiggerlee <xiahulee@gmail.com>
2017-10-11 09:18:15 +0800

    modify exercise 3.2

--- SICP/cp3/exercise3_2.scm
+++ SICP/cp3/exercise3_2.scm
@@ -4,9 +4,13 @@
   (let ((counter 0))
     (lambda (exp)
       (cond ((eq? exp 'how-many-calls?) counter)
+            ((eq? exp 'reset-count)
+             (begin (set! counter 0) counter))
             (else (begin (set! counter (inc counter))
                          (f exp)))))))
 
 (define s (make-monitored sqrt))
 (s 100)              ; 10
 (s 'how-many-calls?) ; 1
+(s 'reset-count)
+(s 'how-many-calls?)
