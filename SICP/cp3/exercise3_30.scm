9f23ffd9004000a5e911c5b925b0b8af4e1aa079
tiggerlee <xiahulee@gmail.com>
2017-10-25 19:16:40 +0800

    exercise 3.30 :shipit:

--- SICP/cp3/exercise3_30.scm
+++ SICP/cp3/exercise3_30.scm
@@ -1,5 +1,11 @@
 #lang sicp
 
+; 延迟时间
+; n 个全加器的延迟时间
+; 一个全加器由两个半加器和一个 or-gate 构成
+; 一个半加器由两个 and-gate 和一个 inverter 一个 or-gate 构成
+; n * (2 * (2 * and-gate-delay + inverter-delay + or-gate-delay) + or-gate-delay)
+
 (define (ripple-carry-adder list-A list-B list-S C)
   (define (iter A B S cin)
     (if (null? A)
