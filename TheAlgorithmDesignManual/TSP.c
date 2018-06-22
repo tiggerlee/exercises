9dd0078b10a90ac88d1bdf7a6fb691a86d3afa3f
tiggerlee <xiahulee@gmail.com>
2018-06-22 17:46:21 +0800

    upload tsp.c

--- TheAlgorithmDesignManual/TSP.c
+++ TheAlgorithmDesignManual/TSP.c
@@ -23,27 +23,22 @@ int findNearestUnvisitedNeighbor(Point points[], Point p, int n)
     for (i = 0; i < n; i++) {
         if (points[i].visited == 1 || p.x == points[i].x && p.y == points[i].y) {
             continue;
-        } else if ((temp_dist = dist(points[i], p)) < min_dist) {
-            min_dist = temp_dist;
-            result = i;
+        } else {
+            temp_dist = dist(points[i], p);
+            if (temp_dist < min_dist) {
+                min_dist = temp_dist;
+                result = i;
+            }
         }
     }
     return result;
 }
 
-void nearestNeighborAlgorithms(Point points[], Point start, int n)
+void nearestNeighborAlgorithms(Point points[], int start_index, int n)
 {
 }
 
 int main(int argc, char *argv[])
 {
-    Point p1 = {2, 0, 0};
-    Point p2 = {18, 0, 0};
-    Point p3 = {4, 0, 0};
-    Point p4 = {7, 0, 0};
-    Point points[] = {p1, p2, p3, p4};
-    printf("Distance is %f\n", dist(p1, p2));
-    printf("Distance is %f\n", dist(p1, p3));
-    printf("Distance is %f\n", dist(p1, p4));
-    printf("p1 nearest point is at %d\n", findNearestUnvisitedNeighbor(points, p1, 4));
+    return 0;
 }
