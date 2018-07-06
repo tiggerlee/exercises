dbbcab144c01e0802bd1284e11ed360da95a5980
tiggerlee <xiahulee@gmail.com>
2018-07-06 14:36:48 +0800

    reverse single linked list

--- TheAlgorithmDesignManual/src/chapter3/StacksQueuesAndLists.kt
+++ TheAlgorithmDesignManual/src/chapter3/StacksQueuesAndLists.kt
@@ -2,6 +2,7 @@ package chapter3
 
 import java.util.*
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 /*
     3-1. A common problem for compilers and text editors is determining whether the parentheses in a string are balanced
@@ -28,19 +29,39 @@ private fun parenthesesBalance(test :String): Int {
         }
     }
     return if (stack.isEmpty()) 0 else stack.pop()
-}
+    }
 
 /*
     3-2. Write a program to reverse the direction of a given singly-linked list. In other words,
      after the reversal all pointers should now point backwards. Your algorithm should take linear time.
  */
 private data class List(val value: Int, var next: List?)
-private fun reverseList(list: List) {
-    
+private fun reverseList(list: List): List {
+    return r(null, list)
+}
+
+private fun r(previous: List?, list: List): List {
+    return if (list.next == null) {
+        list.next = previous
+        list
+    } else {
+        val next = list.next
+        list.next = previous
+        r(list, next!!)
+    }
 }
 
 fun main(args: Array<String>) {
     // test for 3.1
+    testIsParenthesesBalanced()
+
+    // test for 3.2
+    testListWithOneNode()
+    testMoreThanTwoNodes()
+    testListWithTwoNode()
+}
+
+private fun testIsParenthesesBalanced() {
     assertEquals(0, parenthesesBalance(""))
     assertEquals(1, parenthesesBalance("("))
     assertEquals(2, parenthesesBalance("(("))
@@ -49,14 +70,32 @@ fun main(args: Array<String>) {
     assertEquals(1, parenthesesBalance("(()((()))"))
     assertEquals(3, parenthesesBalance("((("))
     assertEquals(3, parenthesesBalance("())"))
+}
 
-    // test for 3.2
+private fun testListWithOneNode() {
+    val node = List(1, null)
+    assertEquals("1", traverseList(node))
+    val reversed = reverseList(node)
+    assertEquals("1", traverseList(reversed))
+}
+
+private fun testListWithTwoNode() {
+    val node1 = List(2, null)
+    val node = List(1, node1)
+    assertEquals("12", traverseList(node))
+    val reversed = reverseList(node)
+    assertEquals("21", traverseList(reversed))
+}
+
+private fun testMoreThanTwoNodes() {
     val node3 = List(3, null)
     val node2 = List(2, node3)
     val node1 = List(1, node2)
-    println(traverseList(node1))
-    println(traverseList(node2))
-    println(traverseList(node3))
+    assertEquals("123", traverseList(node1))
+    assertNotEquals("321", traverseList(node1))
+    val reversed = reverseList(node1)
+    assertNotEquals("123", traverseList(reversed))
+    assertEquals("321", traverseList(reversed))
 }
 
 private fun traverseList(list: List): String {
